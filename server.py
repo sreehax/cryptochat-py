224c4e8c869c2daf21bcb4532b7aaa58d7c4712f
Sreehari S <sreehari@sreehari.dynu.net>
2018-11-23 17:50:51 -0800

    It finally works!

--- server.py
+++ server.py
@@ -11,8 +11,8 @@ class CryptoChatHandler(socketserver.BaseRequestHandler):
             if (self.client_address[0], self.client_address[1]) in dictionary:
                 socket.sendto(b"already exists\n", self.client_address);
             else:
-                socket.sendto(b"Username: ", self.client_address);
                 original = self.client_address
+                socket.sendto(b"Username: ", self.client_address);
                 while True:
                     username = socket.recvfrom(32);
                     if(username[1] == original):
@@ -22,14 +22,15 @@ class CryptoChatHandler(socketserver.BaseRequestHandler):
                 username = username[0]
                 if(username[-1] == 10):
                     username = username[:-1]
-                dictionary[(self.client_address[0], self.client_address[1])] = username
-                print(dictionary)
+                values = list(dictionary.values())
+                if(username in values):
+                    socket.sendto(b"username exists\n", self.client_address);
+                else:
+                    dictionary[(self.client_address[0], self.client_address[1])] = username
         elif(data.startswith(b"kickme")):
             try:
-                #print(self.client_address)
                 del dictionary[(self.client_address[0], self.client_address[1])]
                 socket.sendto(b"success\n", self.client_address);
-                print(dictionary)
             except KeyError:
                 socket.sendto(b"nonexistent\n", self.client_address);
         elif(data.startswith(b"ls")):
@@ -38,6 +39,40 @@ class CryptoChatHandler(socketserver.BaseRequestHandler):
                 s = s.encode()
                 s = s + dictionary[key] + b"\n"
                 socket.sendto(s, self.client_address);
+        elif(data.startswith(b"req")):
+            original = self.client_address
+            socket.sendto(b"Username: ", self.client_address);
+            while True:
+                username = socket.recvfrom(32);
+                if(username[1] == original):
+                    break
+                else:
+                    socket.sendto(fail, username[1])
+            username = username[0]
+            if(username[-1] == 10):
+                username = username[:-1]
+            addr = False
+            keys = list(dictionary.keys())
+            values = list(dictionary.values())
+            i = 0;
+            v = -86;
+            while i < len(values):
+                if values[i] == username:
+                    v = i
+                    break
+                i = i + 1
+            if(v != -86 and keys[v] == (self.client_address[0], self.client_address[1])):
+                socket.sendto(b"That is you\n", self.client_address);
+            elif(v != -86):
+                addr = keys[v]
+                a = addr[0]+","+str(addr[1])+"\n"
+                a = a.encode()
+                msg = b"request-from " + addr[0].encode() + b"," + str(addr[1]).encode() + b"\n"
+                socket.sendto(msg, (addr[0], addr[1]))
+                socket.sendto(a, self.client_address)
+            else:
+                socket.sendto(b"Not Found\n", self.client_address)
+
         else:
             socket.sendto(fail, self.client_address);
 class CryptoChatServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
